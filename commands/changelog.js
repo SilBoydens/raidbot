21d914a5af9a08e6263b027fbb9e41c8f69eb1fe
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2019-06-28 11:17:13 +0430

    Create changelog.js

---
+++ commands/changelog.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'changelog',
+    dm: true,
+    execute(client, msg) {
+      if(!client.config.owners.includes(msg.author.id)) return;
+      client.fetchUser(msg.author.id)
+      .then(dm => {
+        if(client.config.changelog.includes(msg.author.id)) {
+          dm.send('You won\'t receive changelogs anymore. 😦');
+          client.config.changelog.splice(client.config.changelog.indexOf(msg.author.id), 1);
+        } else {
+          dm.send('You will now start receiving changelogs. 🎉');
+          client.config.changelog.push(msg.author.id);
+        }
+        client.util.saveConfig();
+      });
+    }
+}
