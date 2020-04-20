492e8688b7204214bf2c3b8d72d138809b731b79
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2020-04-20 20:13:40 +0430

    compatibility changes, cleanup, fixes

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -1,38 +1,35 @@
-'use strict';
+"use strict";
+
+const img = [
+    "https://zippy.gfycat.com/CarelessSplendidKiskadee.webm",
+    "https://i.gifer.com/7DUg.mp4"
+];
 
 module.exports = {
     guildOnly: true,
-    group: 'serverMod',
-    execute(client, msg, args) {
-        const everyone = msg.guild.roles.get(msg.guild.id);
-        if(!args[0]) {
-            if(everyone.hasPermission("SEND_MESSAGES")) {
-                try {
-                    if (!client.zombie) everyone.setPermissions(everyone.permissions - 0x800);
-                    const links = [
-                        'https://zippy.gfycat.com/CarelessSplendidKiskadee.webm',
-                        'https://i.gifer.com/7DUg.mp4'
-                    ];
-                    msg.reply(`Server locked down\n${links[Math.floor(Math.random()*links.length)]}`);
-                } catch(Exception) {
-                    msg.reply("Something went wrong while locking down\nhttps://i.gifer.com/8urI.mp4");
-                }
+    group: "guildMod",
+    async execute(ctx) {
+        let everyone = ctx.guild.roles.get(ctx.guild.id);
+        if (!ctx.args[0]) {
+            if (everyone.permissions.json.sendMessages) {
+                await everyone.edit({
+                    permissions: everyone.permissions.allow & ~0x800
+                });
+                return `${ctx.user.mention} Server locked down.\n${img[Math.floor(Math.random()*img.length)]}`;
             } else {
-                msg.reply("Server already in lockdown state");
+                return `${ctx.user.mention} Server already in lockdown state.`;
             }
-        } else if (args[0] && args[0].toLowerCase() === 'off') {
-            if(!everyone.hasPermission("SEND_MESSAGES")) {
-                try {
-                    if (!client.zombie) everyone.setPermissions(everyone.permissions + 0x800);
-                    msg.reply("Unlocked the server.");
-                } catch(Exception) {
-                    msg.reply("Something went wrong while unlocking.");
-                }
+        } else if (ctx.args[0] !== undefined && ctx.args[0].toLowerCase() === 'off') {
+            if (!everyone.permissions.json.sendMessages) {
+                await everyone.edit({
+                    permissions: everyone.permissions.allow | 0x800
+                });
+                return `${ctx.user.mention} Unlocked the server.`;
             } else {
-                msg.reply("Can not unlock, server is not locked.");
+                return `${ctx.user.mention} Server is not locked.`;
             }
         } else {
-            msg.reply(`[lockdown] could not understand "${args[0]}"\nUsage:\n\`@${client.user.tag} lockdown\` locks the server down\n\`@${client.user.tag} lockdown off\` unlocks the server`);
+            return `${ctx.user.mention} [lockdown] could not understand "${ctx.args[0]}"\nUsage:\n\`@${this.user.username} lockdown\` locks the server down\n\`@${this.user.username} lockdown off\` unlocks the server`;
         }
     }
 };
