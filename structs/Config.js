69f3836098cc3996258a18f0b0909905e4c35fb9
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2020-04-20 19:55:34 +0430

    improve config saving + bug fix

---
+++ structs/Config.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const housecall = require("housecall");
+const fs        = require("fs");
+const path      = require("path");
+
+class Config {
+    static default = require("../default_config.json");
+    #cache;
+    #queue = housecall({
+        concurrency: 2,
+        cooldown: 1000
+    });
+    constructor(filePath) {
+        this.path   = path.join(process.cwd(), filePath);
+        this.#cache = require(this.path);
+    }
+    get(key) {
+        return this.#cache[key];
+    }
+    createIfNotExists(guildID) {
+        if (this.#cache[guildID]) return;
+        this.#cache[guildID] = Config.default;
+        this.save();
+    }
+    save() {
+        this.#queue.push(() => {
+            fs.writeFile(this.path, JSON.stringify(this.#cache), "utf8", () => {});
+        });
+    }
+    toString() {
+        return `[${this.constructor.name}]`;
+    }
+};
+
+module.exports = Config;
