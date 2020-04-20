202ce5d4f957e4ea53223c17ea8b8535b6ced2ce
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2020-04-20 20:54:14 +0430

    fix leftover from old code

--- commands/changelog.js
+++ commands/changelog.js
@@ -5,11 +5,11 @@ module.exports = {
     group: "botOwner",
     execute(ctx) {
         if (this.config.get("changelog").includes(ctx.user.id)) {
-            this.config.changelog.splice(this.config.get("changelog").indexOf(ctx.user.id), 1);
+            this.config.get("changelog").splice(this.config.get("changelog").indexOf(ctx.user.id), 1);
             this.config.save();
             return `${ctx.user.mention} You won't receive changelogs anymore. 😦`;
         } else {
-            this.config.changelog.push(ctx.user.id);
+            this.config.get("changelog")fi.push(ctx.user.id);
             this.config.save();
             return `${ctx.user.mention} You will now start receiving changelogs. 🎉`;
         }
