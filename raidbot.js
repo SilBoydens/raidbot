0c120a35c3b65b59ad330b92d93e1511feee538e
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2020-06-16 17:11:27 +0430

    remove redundant tasks from READY handler

--- raidbot.js
+++ raidbot.js
@@ -22,15 +22,12 @@ client.on("ready", () => {
     console.log("Serving %d guilds:", client.guilds.size);
 
     for (let [, guild] of client.guilds) {
-        client.createTable(guild.id);
         console.log("* %s", guild.name);
     }
     
     client.editStatus(void(0), {
         name: "Helping moderators"
     });
-    // clean the sqlite db every hour
-    setInterval(client.flushDB, 3600000);
 });
 
 client.connect();
