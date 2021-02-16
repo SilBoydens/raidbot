c465743d33484675a11e038fb0f843998fbc85c5
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2021-02-17 00:04:09 +0330

    Update raidbot.js

--- raidbot.js
+++ raidbot.js
@@ -7,7 +7,6 @@ require("dotenv").config();
 */
 
 const RaidBot = require("./structs/RaidBot"), client = new RaidBot(process.env.RAIDBOT_TOKEN, {
-    getAllUsers: true,
     defaultImageSize: 512,
     defaultImageFormat: "png",
     restMode: true,
