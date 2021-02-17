ac0f2b7c3f8591b51c32fdd214317b37c3ac878a
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2021-02-17 10:21:24 +0330

    update intents

--- raidbot.js
+++ raidbot.js
@@ -7,11 +7,13 @@ require("dotenv").config();
 */
 
 const RaidBot = require("./structs/RaidBot"), client = new RaidBot(process.env.RAIDBOT_TOKEN, {
+    getAllUsers: true,
     defaultImageSize: 512,
     defaultImageFormat: "png",
     restMode: true,
     intents: [
-        "guilds", "guildMessages", "directMessages"
+        "guilds", "guildMembers", "guildMessages",
+        "directMessages"
     ]
 }, {
     dbFile: process.env.DB_FILE,
