ba2c50c8ba14ba3e6fddc8d9053eb3445e5b7bdb
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2021-02-17 13:23:56 +0330

    allow users to supply durations more conveniently

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -12,14 +12,18 @@ module.exports = {
         if (seconds === undefined) {
             return `${ctx.user.mention} Could not find enough arguments.\n**Usage**: \`${ctx.command.usage}\``;
         }
-        seconds = utils.parseDuration(seconds.toLowerCase() === "off" ? 0 : seconds);
-        if (seconds === null) {
-            return "Invalid duration specified, examples of acceptables durations are: `3`, `4h`, `3h30m` etc.";
+        if (["0", "off"].includes(seconds.toLowerCase())) {
+            seconds = 0;
         } else {
-            seconds = seconds.total;
+            seconds = utils.parseDuration(seconds);
+            if (seconds === null) {
+                return "Invalid duration specified, examples of acceptables durations are: `3`, `4h`, `3h30m` etc.";
+            } else {
+                seconds = seconds.total;
+            }
         }
-        if (seconds < 0 || seconds > 21600 || !Number.isInteger(seconds)) {
-            return `${ctx.user.mention} Invalid timeout specified, timeout must be a number between \`0\` and \`21600\`.`;
+        if (seconds > 216E2) {
+            return "Invalid timeout specified, timeout must be a number between \`0\` and \`21600\`.";
         } else if (seconds === ctx.channel.rateLimitPerUser) {
             return seconds ? `${ctx.channel.mention} already has slowmode enabled with the specified timeout.` : `${ctx.channel.mention} does not have slowmode enabled.`;
         }
