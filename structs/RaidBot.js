648c05f328529fe0f7a2a24b5989a332ff056930
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2021-02-17 11:27:19 +0330

    Update RaidBot.js

--- structs/RaidBot.js
+++ structs/RaidBot.js
@@ -52,7 +52,7 @@ class RaidBot extends Eris.Client {
         
         this.flushDBTimer = setInterval(() => {
             for (let id of this._guilds) {
-                let stmt = this.db.prepare(`DELETE FROM g${id} WHERE timestamp < ${new Date(new Date().getTime() - (36E5)).getTime()}`).on(_handleError);
+                let stmt = this.db.prepare(`DELETE FROM g${id} WHERE timestamp < ${new Date(new Date().getTime() - (36E5)).getTime()}`).on("error", _handleError);
                 stmt.run();
             }
         }, 36E5); // clean the sqlite db every hour
