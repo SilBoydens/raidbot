d0fd06a4213a225d72bd8893b5fddef5d819a545
Alireza Rafiee <38037005+Alirezarfe@users.noreply.github.com>
2021-02-17 13:19:48 +0330

    allow users to supply durations more conveniently

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const utils = require("../utils");
+
 module.exports = {
     guildOnly: true,
     level: "guild_mod",
@@ -10,7 +12,12 @@ module.exports = {
         if (seconds === undefined) {
             return `${ctx.user.mention} Could not find enough arguments.\n**Usage**: \`${ctx.command.usage}\``;
         }
-        seconds = seconds.toLowerCase() === "off" ? 0 : +seconds;
+        seconds = utils.parseDuration(seconds.toLowerCase() === "off" ? 0 : seconds);
+        if (seconds === null) {
+            return "Invalid duration specified, examples of acceptables durations are: `3`, `4h`, `3h30m` etc.";
+        } else {
+            seconds = seconds.total;
+        }
         if (seconds < 0 || seconds > 21600 || !Number.isInteger(seconds)) {
             return `${ctx.user.mention} Invalid timeout specified, timeout must be a number between \`0\` and \`21600\`.`;
         } else if (seconds === ctx.channel.rateLimitPerUser) {
